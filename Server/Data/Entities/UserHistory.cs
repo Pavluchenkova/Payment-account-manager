68aa415bfe504d77c57196f872584c1ac066d43e
Agnessa Pavlyuchenkova <agnessa.pavlyuchenkova@gmail.com>
2017-02-25 23:44:52 +0200

    Create server

---
+++ Server/Data/Entities/UserHistory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+   public class UserHistory
+    {
+        public Guid UserHistoryId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public decimal Balance { get; set; }
+        public int AccountNumber { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ModificationDate { get; set; }
+        public DateTime ValidTillDate { get; set; }
+        public DateTime IncomeDate { get; set; }
+        public string Notes { get; set; }
+
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Server/Repositories/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Repositories
+{
+    public class UserRepository
+    {
+        private readonly UserContext _dbContext;
+
+        public UserRepository(UserContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public User GetByAccountNumber(int accountNumber)
+        {
+            var entity = _dbContext.Set<User>().FirstOrDefault(e => e.AccountNumber == accountNumber);
+
+            return entity;
+        }
+
+        public void Update(User entity)
+        {
+            entity.ModificationDate = DateTime.Now;
+            entity.UserHistories.Add(new UserHistory
+            {
+                UserHistoryId = Guid.NewGuid(),
+                UserId = entity.UserId,
+                Name = entity.Name,
+                Phone = entity.Phone,
+                Balance = entity.Balance,
+                AccountNumber = entity.AccountNumber,
+                CreationDate = entity.CreationDate,
+                ModificationDate = entity.ModificationDate,
+                ValidTillDate = entity.ValidTillDate,
+                IncomeDate = entity.IncomeDate,
+                Notes = entity.Notes,
+            });
+
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+        }
+
+    }
+}
